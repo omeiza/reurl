0af3ea3680db3f56812874b6141bfd3432ee3d05
Omeiza Owuda <owudaomeiza@gmail.com>
2023-04-03 22:10:58 +0100

    User model setup

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -17,7 +17,35 @@ const User = sequelize.define(
             primaryKey: true
         },
         email: {
-
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                isEmail: true
+            }
+        },
+        passwordHash: {
+            type: DataTypes.STRING,
+            allowNull: true,
+            set(value) {
+                // this.setDataValue('passwordHash', hash(value))
+            }
+        },
+        username: {
+            type: DataTypes.STRING,
+            unique: true,
+            allowNull: false
+        },
+        apiKey: {
+            type: DataTypes.STRING,
+            allowNull: true
+        },
+        isAdmin: {
+            type: DataTypes.BOOLEAN,
+            defaultValue: false
+        },
+        isActive: {
+            type: DataTypes.BOOLEAN,
+            defaultValue: false
         }
     }
 )
