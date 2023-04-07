484c0c078add1f12d965f63d1fdafa2b0ebd6ccf
Omeiza Owuda <owudaomeiza@gmail.com>
2023-04-07 21:32:31 +0100

    Models sync setup

---
+++ scripts/syncDB.js
@@ -0,0 +1,37 @@
+/**
+ * Sync in all models to create and update databases
+ * Author: https://github.com/omeiza
+ */
+
+require('../src/utils/env.util');
+const User = require('../src/models/user.model');
+const Link = require('../src/models/link.model');
+const AuthService = require('../src/models/authService.model');
+
+try {
+	/**
+	 * 1. Sync (Create or Update) in User model
+	 * 2. Sync (Create or Update) in AuthService model
+	 * 3. Setup model associations between User & AuthService
+	 */
+	await User.sync({ alter: true });
+	await AuthService.sync({ alter: true })
+		.then(() => {
+			User.hasOne(AuthService);
+			AuthService.belongsTo(User);
+		});
+
+	/**
+	 * 1. Sync (Create or Update) in Link model
+	 * 2. Setup model association between Link & User
+	 */
+	await Link.sync({ alter: true })
+		.then(async() => {
+			User.hasMany(Link);
+			Link.belongsTo(User);
+		});
+
+	console.log("Databases tables successfully synced in!");
+} catch (error) {
+	console.log("Error syncing databases tables", error);
+}
