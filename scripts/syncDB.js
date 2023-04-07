9eabae88963f5d892d326418ecf937393073a7d6
Omeiza Owuda <owudaomeiza@gmail.com>
2023-04-07 21:58:36 +0100

    Wrap models sync implementation with IIFE function

--- scripts/syncDB.js
+++ scripts/syncDB.js
@@ -1,6 +1,7 @@
 /**
  * Sync in all models to create and update databases
  * Author: https://github.com/omeiza
+ * @TODO: Exit process after running script
  */
 
 require('../src/utils/env.util');
@@ -8,30 +9,32 @@ const User = require('../src/models/user.model');
 const Link = require('../src/models/link.model');
 const AuthService = require('../src/models/authService.model');
 
-try {
-	/**
-	 * 1. Sync (Create or Update) in User model
-	 * 2. Sync (Create or Update) in AuthService model
-	 * 3. Setup model associations between User & AuthService
-	 */
-	await User.sync({ alter: true });
-	await AuthService.sync({ alter: true })
-		.then(() => {
-			User.hasOne(AuthService);
-			AuthService.belongsTo(User);
-		});
+(async () => {
+	try {
+		/**
+		 * 1. Sync (Create or Update) in User model
+		 * 2. Sync (Create or Update) in AuthService model
+		 * 3. Setup model associations between User & AuthService
+		 */
+		await User.sync({ alter: true });
+		await AuthService.sync({ alter: true })
+			.then(() => {
+				User.hasOne(AuthService);
+				AuthService.belongsTo(User);
+			});
 
-	/**
-	 * 1. Sync (Create or Update) in Link model
-	 * 2. Setup model association between Link & User
-	 */
-	await Link.sync({ alter: true })
-		.then(async() => {
-			User.hasMany(Link);
-			Link.belongsTo(User);
-		});
+		/**
+		 * 1. Sync (Create or Update) in Link model
+		 * 2. Setup model association between Link & User
+		 */
+		await Link.sync({ alter: true })
+			.then(async() => {
+				User.hasMany(Link);
+				Link.belongsTo(User);
+			});
 
-	console.log("Databases tables successfully synced in!");
-} catch (error) {
-	console.log("Error syncing databases tables", error);
-}
+		console.log("Database tables successfully synced in!");
+	} catch (error) {
+		console.log("Error syncing databases tables", error);
+	}
+})();
