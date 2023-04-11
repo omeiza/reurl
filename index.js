459d221f9693d7c21008082f076158841a23d1b3
Omeiza Owuda <owudaomeiza@gmail.com>
2023-04-11 23:42:59 +0100

    Initial routing setup

--- index.js
+++ index.js
@@ -3,11 +3,40 @@ const express = require('express');
 
 const app = express();
 const port = process.env.PORT ?? '3001';
+const models = require('./src/models');
 
-app.get('/', (req, res) => {
-	res.send('Express');
+/**
+ * Application Main Routes
+ * 1. Users
+ * 2. Links
+ * 3. Authentication
+ */
+app.use('/users');
+app.use('/links');
+app.use('/auth');
+
+/**
+ * Error handling with express app middleware
+ */
+app.use((error, req, res, next) => {
+	res.status(error.status || 500).json({
+		status: error.message,
+		errors: error.errors
+	});
 });
 
-app.listen(port, () => {
-	console.log(`[server]: Server is running at http://localhost:${port}`);
-});
+(async () => {
+	try {
+		models.sequelize
+			.authenticate()
+			.then(() => {
+				console.log('Database connection has been established successfully');
+
+				app.listen(port, () => {
+					console.log(`Shortner app listening on port ${port}`);
+				});
+			});
+	} catch (error) {
+		console.error('Unable to connect to database -> ', error.original);
+	}
+})();
