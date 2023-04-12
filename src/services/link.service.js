6a9431b4c826cc09a7f4a69c63ee19104d22689e
Omeiza Owuda <owudaomeiza@gmail.com>
2023-04-12 14:17:37 +0100

    Started link services

---
+++ src/services/link.service.js
@@ -0,0 +1,30 @@
+/**
+ * Name: Link services
+ * Author: https://github.com/omeiza
+ * 1. Create
+ * 2. Read
+ * 3. Update
+ * 4. Delete
+ */
+
+/**
+ * Create a new link
+ *
+ * @param {number} userId
+ * @param {string} url
+ * @return {(number | null)} Id of the created link or null
+ */
+
+/**
+ * Get an existing link
+ *
+ * @param {number} linkId
+ * @return {Object | null} Data of the queried link or null
+ */
+
+/**
+ * Update an existing link
+ *
+ * @param {number} linkId
+ * @param {Object} data
+ */
