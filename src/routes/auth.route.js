077c7a2da5041c85fc5b0be4f1bbfafb62d78140
Omeiza Owuda <owudaomeiza@gmail.com>
2023-04-29 00:29:04 +0100

    Auth route comments

--- src/routes/auth.route.js
+++ src/routes/auth.route.js
@@ -1,11 +1,11 @@
+/**
+ * Authentication routes
+ * Author: https://github.com/omeiza
+ */
+
 const express = require('express');
 const userController = require("../controllers/users.controller");
 const router = express.Router();
-const authenticate = require("../middlewares/authenticate.middleware");
-
-/**
- * Authentication routes to their controllers
- */
 
 // Add new user
 router.put('/signup', userController.signup);
